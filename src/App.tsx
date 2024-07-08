557fcae054906724c53d41c4160522f1fe066c41
mhmd-salah <ms.mhmdsalah@gmail.com>
2024-07-09 01:19:14 +0200

    create router component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import router from "./router/router";
 function App() {
   return (
     <>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
     </>
   );
 }
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,19 +1,18 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
-// import Navbar from "../components/Navbar";
+import { createBrowserRouter, createRoutesFromElements, Outlet, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      {/* <Navbar home="home" /> */}
-      <Route path="/" element={<h1>home page</h1>}>
-        <Route path="content" element={<h1>content page</h1>} />
-        <Route path="about" element={<h1>about page</h1>} />
+      <Route path="/" element={<div>
+        <Navbar home="home"/>
+        <h1>root layout</h1>
+        <Outlet/>
+      </div>} >
+        <Route path="content" element={<h1>content page</h1>}/>
+        <Route path="products" element={<h1>products page</h1>}/>
       </Route>
     </>
   )
-);
+)
 export default router;
